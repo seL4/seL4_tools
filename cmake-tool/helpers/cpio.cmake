2eb669c6b133ced621733077016400edd5fd944f
Jashank Jeremy <jashank.jeremy@unsw.edu.au>
2020-11-13 00:28:43 +1100

    cmake,cpio: Move cpio(5) archive generation mechanism into script
    Move the process of constructing an archive out into a new Python script
    `make_cpio.py', and simplify the mechanism by directly using libarchive
    to generate a file with the desired properties.
    
    (I've previously run afoul of GNU cpio(1)'s `--reproducible' flag and
    the detection machinery thereof; and of subtle and tricky-to-debug build
    failures that occur partway through the generated command line.  I opted
    to rework the CMake into Python; the result is that the CMake here
    simplifies to the invocation of a single, fairly simple tool with
    appropriate arguments, instead of the composition of a fairly involved
    shell script.)
    
    r2: `cmake-format'.
    
    r3: rebased, and reworded commit message to more clearly explain the
        rationale for the change, as this was open to misinterpretation.
    
    Signed-off-by: Jashank Jeremy <jashank.jeremy@unsw.edu.au>

--- cmake-tool/helpers/cpio.cmake
+++ cmake-tool/helpers/cpio.cmake
@@ -9,28 +9,10 @@
 
 include_guard(GLOBAL)
 
-# Checks the existence of an argument to cpio -o.
-# flag refers to a variable in the parent scope that contains the argument, if
-# the argument isn't supported then the flag is set to the empty string in the parent scope.
-function(CheckCPIOArgument var flag)
-    if(NOT (DEFINED ${var}))
-        file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/cpio-testfile "Testfile contents")
-        execute_process(
-            COMMAND bash -c "echo cpio-testfile | cpio ${flag} -o"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            OUTPUT_QUIET ERROR_QUIET
-            RESULT_VARIABLE result
-        )
-        if(result)
-            set(${var} "" CACHE INTERNAL "")
-            message(STATUS "CPIO test ${var} FAILED")
-        else()
-            set(${var} "${flag}" CACHE INTERNAL "")
-            message(STATUS "CPIO test ${var} PASSED")
-        endif()
-        file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/cpio-testfile)
-    endif()
-endfunction()
+set(MAKE_CPIO_TOOL
+    "${CMAKE_CURRENT_LIST_DIR}/make_cpio.py"
+    CACHE INTERNAL "" FORCE
+)
 
 # Function for declaring rules to build a cpio archive that can be linked
 # into another target
@@ -43,25 +25,7 @@ function(MakeCPIO output_name input_files)
     if(NOT "${MAKE_CPIO_CPIO_SYMBOL}" STREQUAL "")
         set(archive_symbol ${MAKE_CPIO_CPIO_SYMBOL})
     endif()
-    # Check that the reproducible flag is available. Don't use it if it isn't.
-    CheckCPIOArgument(cpio_reproducible_flag "--reproducible")
-    set(append "")
-    set(
-        commands
-        "bash;-c;cpio ${cpio_reproducible_flag} --quiet --create -H newc --file=${CMAKE_CURRENT_BINARY_DIR}/archive.${output_name}.cpio;&&"
-    )
-    foreach(file IN LISTS input_files)
-        # Try and generate reproducible cpio meta-data as we do this:
-        # - touch -d @0 file sets the modified time to 0
-        # - --owner=root:root sets user and group values to 0:0
-        # - --reproducible creates reproducible archives with consistent inodes and device numbering
-        list(
-            APPEND
-                commands
-                "bash;-c; mkdir -p temp_${output_name} && cd temp_${output_name} && cp -a ${file} . && touch -d @0 `basename ${file}` && echo `basename ${file}` | cpio --append ${cpio_reproducible_flag} --owner=+0:+0 --quiet -o -H newc --file=${CMAKE_CURRENT_BINARY_DIR}/archive.${output_name}.cpio && rm `basename ${file}` && cd ../ && rmdir temp_${output_name};&&"
-        )
-    endforeach()
-    list(APPEND commands "true")
+
     separate_arguments(cmake_c_flags_sep NATIVE_COMMAND "${CMAKE_C_FLAGS}")
     if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
         list(APPEND cmake_c_flags_sep "${CMAKE_C_COMPILE_OPTIONS_TARGET}${CMAKE_C_COMPILER_TARGET}")
@@ -70,18 +34,16 @@ function(MakeCPIO output_name input_files)
     add_custom_command(
         OUTPUT ${output_name}
         COMMAND rm -f archive.${output_name}.cpio
-        COMMAND ${commands}
+        COMMAND ${MAKE_CPIO_TOOL} ${CMAKE_CURRENT_BINARY_DIR}/archive.${output_name}.cpio
+                ${input_files}
         COMMAND
             sh -c
             "echo 'X.section ._archive_cpio,\"aw\"X.globl ${archive_symbol}, ${archive_symbol}_endX${archive_symbol}:X.incbin \"archive.${output_name}.cpio\"X${archive_symbol}_end:X' | tr X '\\n'"
             > ${output_name}.S
-        COMMAND
-            ${CMAKE_C_COMPILER} ${cmake_c_flags_sep} -c -o ${output_name} ${output_name}.S
+        COMMAND ${CMAKE_C_COMPILER} ${cmake_c_flags_sep} -c -o ${output_name} ${output_name}.S
         DEPENDS ${input_files} ${MAKE_CPIO_DEPENDS}
         VERBATIM
-        BYPRODUCTS
-        archive.${output_name}.cpio
-        ${output_name}.S
+        BYPRODUCTS archive.${output_name}.cpio ${output_name}.S
         COMMENT "Generate CPIO archive ${output_name}"
     )
 endfunction(MakeCPIO)
