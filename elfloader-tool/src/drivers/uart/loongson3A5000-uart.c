a3209535f7966c003af67110e8fdf09eead6f766
Yang Chen <ricky.yang.chen@gmail.com>
2023-05-22 16:57:32 +0800

    uart: clean debug code

--- elfloader-tool/src/drivers/uart/loongson3A5000-uart.c
+++ elfloader-tool/src/drivers/uart/loongson3A5000-uart.c
@@ -28,13 +28,6 @@ static int loongson3A5000_uart_putchar(struct elfloader_device *dev, unsigned in
 {
     volatile void *mmio = dev->region_bases[0];
     while (!(*UART_REG(mmio, UART_REG_LSR) & UART_REG_LSR_TE));
-    asm volatile(
-        "csrwr  $t0, 0x34  \n"
-        "csrwr  $t1, 0x35  \n"
-        "li.d  $t1, 0x1fe001e5  \n"
-        "iocsrrd.b $t0, $t1  \n"
-        "csrrd  $t0, 0x34  \n"
-        "csrrd  $t1, 0x35  \n");
     *UART_REG(mmio, UART_REG_DAT) = (c & 0xff);
 
     return 0;
